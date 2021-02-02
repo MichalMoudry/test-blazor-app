4b7618ae13be561115d6dec7d42672c8a72e3f3a
MichalMoudry <moudry.michal@live.com>
2021-02-02 20:38:02 +0100

    Changed file input system

--- FormCapture/Client/wwwroot/js/main.js
+++ FormCapture/Client/wwwroot/js/main.js
@@ -12,30 +12,6 @@ function uncheckCheckboxes(checkboxIdArray) {
     }
 }
 
-function getImageProperties(imageID) {
-    var img = document.getElementById(imageID);
-    return img.naturalWidth + "|" + img.naturalHeight;
-}
-
-function readUploadedFilesAsText(input) {
-    const temporaryFileReader = new FileReader();
-    return new Promise((resolve, reject) => {
-        temporaryFileReader.onerror = () => {
-            temporaryFileReader.abort();
-            reject(new DOMException("Problem parsing input file."));
-        };
-        temporaryFileReader.addEventListener("load", function () {
-            var data = temporaryFileReader.result.split(',')[1];
-            resolve(data);
-        }, false);
-        temporaryFileReader.readAsDataURL(input.files[0]);
-    });
-}
-
-function getFileData(inputID) {
-    return readUploadedFilesAsText(document.getElementById(inputID));
-}
-
 function drawField() {
     var canvas = document.getElementById("template-canvas");
     console.log(canvas);
